79562b85fa5ce88450b7c6a27b529a193f16ad6a
satrianugrohoputra <satrianugrohoputra.xa6@gmail.com>
2025-07-21 01:30:34 +0700

    Update dimsum Image

--- src/components/MenuSection.tsx
+++ src/components/MenuSection.tsx
@@ -33,7 +33,7 @@ const MenuSection = () => {
     {
       id: 3,
       name: "Dimsum",
-      image: "https://images.unsplash.com/photo-1563245370-63ffc97abdbd?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      image: "/images/dimsum.jpg",
       slicePrice: "",
       wholePrice: "Rp 175.000 (50pcs)",
       description: "Dimsum dengan rasa yang lezat dan nikmat, sudah include sausnya"
